7c950bf7282e1383e2e7fb6cd8e1b30574aea235
Aleksey Zhidkov <me@azhidkov.pro>
2024-08-25 16:43:56 +0700

    Добавлен скелет проекта

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.gradle.ext.packagePrefix
+import org.jetbrains.gradle.ext.settings
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0"
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.8"
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "pro.azhidkov.training"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+idea {
+    module {
+        settings {
+            // Для того чтобы идея подхватывала схему пакетов по конвенции без общего префикса
+            packagePrefix["src/main/kotlin"] = "pro.azhidkov.training.project_sherlok"
+            packagePrefix["src/test/kotlin"] = "pro.azhidkov.training.project_sherlok"
+        }
+    }
+}
---
+++ src/main/kotlin/ProjectSherlokApp.kt
@@ -0,0 +1,11 @@
+package pro.azhidkov.training.project_sherlok
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectSherlokApp
+
+fun main(args: Array<String>) {
+    runApplication<ProjectSherlokApp>(*args)
+}
